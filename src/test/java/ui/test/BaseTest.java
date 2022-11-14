ac6f27596c5b21a9867b3158b4a67251362a7a33
Kirill Vasilenko <41346780+wetanos@users.noreply.github.com>
2022-11-14 16:17:55 +0400

    refactor/adding-beforetest

--- src/test/java/ui/test/BaseTest.java
+++ src/test/java/ui/test/BaseTest.java
@@ -1,12 +1,18 @@
 package ui.test;
 
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import ui.driver.SingletonWebDriver;
 import ui.utils.TestListener;
 
 @Listeners(TestListener.class)
 public class BaseTest {
+
+    @BeforeTest
+    public void startTest() {
+        SingletonWebDriver.getDriver();
+    }
 
     @AfterTest
     public void finishTest() {
